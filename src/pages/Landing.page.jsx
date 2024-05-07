271adb9fd96f098fe30c5f9bd31dc6f23d0c541c
kuang-yu-wang <211410146@o365.tku.edu.tw>
2024-05-07 20:58:03 +0800

    feat: landing page
    seperate dashboard and landing page

---
+++ src/pages/Landing.page.jsx
@@ -0,0 +1,8 @@
+import Hero from "../components/sections/Hero";
+const Landing = () => (
+  <>
+    <Hero title="Chrono Dump" subtitle="Organized your time, making you producitvity." />
+  </>
+)
+
+export default Landing;
